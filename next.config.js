888faef0b19cc22cf73a4da61bf803dc856a7fd0
xiawang1024 <xiawang1024@gmail.com>
2021-04-23 18:05:37 +0800

    ✨feat: 构建配置

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  compress: false, //禁止gzip压缩，转移到nginx上
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+  },
+  generateBuildId: async () => {
+    return 'nextssrhntv0' //构建ID
   },
 }
