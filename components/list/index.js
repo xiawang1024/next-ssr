ad1dc04b31f3488206bb41aa9138b715010bdf17
xiawang1024 <xiawang1024@gmail.com>
2021-04-26 17:41:16 +0800

    ✨feat: useRouter

--- components/list/index.js
+++ components/list/index.js
@@ -1,40 +1,41 @@
 import Link from 'next/link'
 import styles from 'components/list/list.module.scss'
+import { useRouter } from 'next/router'
 
 function ArticleList({ articles }) {
+  const router = useRouter()
   function filterCover(item) {
     let { coverImagesList } = item
     let coverInfo = coverImagesList.filter((item) => item.coverBool === true)[0]
 
-    console.log(coverInfo)
     if (coverInfo) {
       return coverInfo
     } else {
       return coverImagesList[0]
     }
   }
-  console.log(filterCover(articles[0]))
+
   return (
     <div className={styles.listWrap}>
       {articles &&
         articles.map((item) => (
-          <div key={item.id}>
-            <Link href={`/cmsid/${encodeURIComponent(item.id)}`}>
-              <a className={styles.item}>
-                <div className={styles.imgWrap}>
-                  {filterCover(item)?.url && (
-                    <img className={styles.img} src={filterCover(item).url} />
-                  )}
-                </div>
-                <div className={styles.textWrap}>
-                  <h3 className={styles.title}>{item.title}</h3>
-                  <p className={styles.other}>
-                    <span className={styles.time}>{item.publishDate}</span>
-                    <span className={styles.origin}>{item.origin}</span>
-                  </p>
-                </div>
-              </a>
-            </Link>
+          <div
+            key={item.id}
+            className={styles.item}
+            onClick={() => router.push(`/cmsid/${encodeURIComponent(item.id)}`)}
+          >
+            <div className={styles.imgWrap}>
+              {filterCover(item)?.url && (
+                <img className={styles.img} src={filterCover(item).url} />
+              )}
+            </div>
+            <div className={styles.textWrap}>
+              <h3 className={styles.title}>{item.title}</h3>
+              <p className={styles.other}>
+                <span className={styles.time}>{item.publishDate}</span>
+                <span className={styles.origin}>{item.origin}</span>
+              </p>
+            </div>
           </div>
         ))}
     </div>
